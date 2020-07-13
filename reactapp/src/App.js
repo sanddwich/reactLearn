963224a076131a35f9af683b6ac6bf04d67f91fa
sanddwich <64834314+sanddwich@users.noreply.github.com>
2020-07-13 17:02:00 +0400

    13-07-2020_1

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,24 +1,80 @@
-import React from 'react';
-// import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
-import Car from './Car/Car';
+import Car from './Car/Car'
 
-function App() {
-  const divStyle = {
-    textAlign: 'center',
-    color: 'green',
-  };
+class App extends Component {
 
-  return (
-    <div style={divStyle}>
-      <h1>Hello, world!!!</h1>
-      <Car name={"Ford"} year={2019} />
-      <Car name={"Mazda"} year={2015} />
-      <Car name={"Somebody"} year={2014}>
-        <p>COLOR</p>
-      </Car>
-    </div>
-  );
+  state = {
+    cars: [
+      {name: 'Ford', year: 2018},
+      {name: 'Audi', year: 2016},
+      {name: 'Mazda', year: 2010}
+    ],
+    pageTitle: 'React components',
+    showCars: false
+  }
+
+  toggleCarsHandler = () => {
+    this.setState({
+      showCars: !this.state.showCars
+    })
+  }
+
+  onChangeName(name, index) {
+    const car = this.state.cars[index]
+    car.name = name
+    const cars = [...this.state.cars]
+    cars[index] = car
+    this.setState({cars})
+  }
+
+  deleteHandler(index) {
+    const cars = this.state.cars.concat()
+    cars.splice(index, 1)
+
+    this.setState({cars})
+
+  }
+
+  render() {
+    const divStyle = {
+      textAlign: 'center'
+    }
+
+    let cars = null
+
+    if (this.state.showCars) {
+      cars = this.state.cars.map((car, index) => {
+        return (
+          <Car
+            key={index}
+            name={car.name}
+            year={car.year}
+            onDelete={this.deleteHandler.bind(this, index)}
+            onChangeName={event => this.onChangeName(event.target.value, index)}
+          />
+        )
+      })
+    }
+
+    return (
+      <div style={divStyle}>
+        <h1>{this.state.pageTitle}</h1>
+
+        <button
+          onClick={this.toggleCarsHandler}
+        >Toggle cars</button>
+
+        <div style={{
+          width: 400,
+          margin: 'auto',
+          paddingTop: '20px'
+        }}>
+          { cars }
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
--- reactapp/src/Car/Car.js
+++ reactapp/src/Car/Car.js
@@ -1,25 +1,44 @@
-import React from 'react';
+import React from 'react'
+import Radium from 'radium'
+import './Car.css'
 
-// function Car() {
-//   return (
-//     <h2>Car component</h2>
-//   )
-// }
+const Car = props => {
+  const inputClasses = ['input']
 
-// const car = () => {
-//   return (
-//     <h2>Car component</h2>
-//   )
-// }
+  if (props.name !== '') {
+    inputClasses.push('green')
+  } else {
+    inputClasses.push('red')
+  }
 
-// const car = () => <h2>Car component</h2>;
+  if (props.name.length > 4) {
+    inputClasses.push('bold')
+  }
 
-export default props => (
-  <div>
-    <h2>Car component</h2>
-    <p>{Math.random()}</p>
-    <p>Car name: {props.name}</p>
-    <p>Year: {props.year}</p>
-    {props.children}
-  </div>
-);
+  const style = {
+    border: '1px solid #ccc',
+    boxShadow: '0 4px 5px 0 rgba(0, 0, 0, .14)',
+    ':hover': {
+      border: '1px solid #aaa',
+      boxShadow: '0 4px 15px 0 rgba(0, 0, 0, .25)',
+      cursor: 'pointer'
+    }
+  }
+
+  return (
+    <div className="Car" style={style}>
+      <h3>Сar name: {props.name}</h3>
+      <p>Year: <strong>{props.year}</strong></p>
+      <input
+        type="text"
+        onChange={props.onChangeName}
+        value={props.name}
+        className={inputClasses.join(' ')}
+      />
+      <button onClick={props.onDelete}>Delete</button>
+    </div>
+  )
+}
+
+
+export default Radium(Car)
