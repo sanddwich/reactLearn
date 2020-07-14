aa4545a5fefdad288efeb737a647b58fa71ee700
sanddwich <64834314+sanddwich@users.noreply.github.com>
2020-07-14 15:17:38 +0400

    14-07-2020_5

---
+++ react-quiz/src/hoc/Layouts/Layout.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import classes from './Layout.module.scss';
+import Quiz from '../../containers/Quiz/Quiz';
+
+class Layout extends Component {
+  render() {
+    console.log(classes);
+    return (
+      <div className={classes.Layout}>
+        <h1>mainLayout</h1>
+
+        <main>
+          <Quiz />
+        </main>
+      </div>
+    );
+  }
+}
+
+export default Layout;
